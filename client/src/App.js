48b4014e3cccbf210e97d3638e4a8c23d4ae6ac8
Igor Povar <goshuchok@gmail.com>
2022-09-07 10:50:48 +0300

    add grid

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { CssBaseline, Container } from '@mui/material';
+import { CssBaseline, Container, Box } from '@mui/material';
 
 import { Navigation } from './components';
 import { Home, Recommend, Settings } from './pages';
@@ -9,13 +9,19 @@ function App() {
     <BrowserRouter>
       <CssBaseline />
       <Navigation />
-      <Container maxWidth="xl">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="settings" element={<Settings />} />
-          <Route path="recommend" element={<Recommend />} />
-        </Routes>
-      </Container>
+      <Box
+        sx={{
+          backgroundColor: (theme) => theme.palette.grey[100],
+        }}
+      >
+        <Container maxWidth="xl">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="settings" element={<Settings />} />
+            <Route path="recommend" element={<Recommend />} />
+          </Routes>
+        </Container>
+      </Box>
     </BrowserRouter>
   );
 }
